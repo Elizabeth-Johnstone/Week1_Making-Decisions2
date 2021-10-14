8558eea59aeb6f2cde38c86a80fc713a1d84b52b
Elizabeth Johnstone <ewj218@nyu.edu>
2021-10-14 15:51:21 -0400

    assignment completed

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -113,9 +113,14 @@ let someNum = myFavoriteNumbers[4]
 var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Do not edit the code above.
 
-// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
+// Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3.
+// // If it is, console.log '{number} is divisible by 3.'
 
-//Code Here
+// for (let i = 0; i < listOfNumbers.length; i++) {
+//   if (listOfNumbers[i] % 3 === 0) {
+//     console.log(listOfNumbers[i] + " is divisible by 5")
+//   }
+// }
 
 
 ////////// PROBLEM 9 //////////
@@ -123,20 +128,23 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 var letters = ['A', 'B', 'C', 'D', 'E'];
 // Do not edit the code above.
 
-/*
-  Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
-*/
+// /*
+//   Loop backwards, starting at the end of the 'letters' array. Console log every item in the array.
+// */
 
-//Code Here
+// for (let i = letters.length - 1; i >= 0; i--) {
+//   console.log(letters[i])
+// }
 
 
 ////////// Advanced Problems //////////
 
 ////////// PROBLEM 10 //////////
-// Switch statements can be excellent alternatives to if blocks. Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
+// Switch statements can be excellent alternatives to if blocks.
+// Look up switch statements (I recommend W3 Schools) and try to implement one for the following.
 
 // Do not edit the code below.
-let letterGrade = 'B'
+let letterGrade = 'G'
 // Do not edit the code above.
 
 /* Use a switch statement on 'letterGrade' and console.log the appropriate response.
@@ -149,14 +157,35 @@ If F: "The student is failing."
 If the letter grade is not one of the above letters, console.log 'Not an eligible grade.'
 */
 
-//Code Here
+switch(letterGrade) {
+  case 'A':
+  console.log("The student is doing excellently.");
+  break;
+  case 'B':
+  console.log("The student is doing well.");
+  break;
+  case 'C':
+  console.log("The student is doing alright.")
+  break;
+  case 'D':
+  console.log("The student is not doing very well.")
+  break;
+  case 'F':
+  console.log("The student is failing.")
+  break;
+  default:
+    console.log('Not an eligible grade.')
+
+}
 
 
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
-  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic, if the number your for loop is currently on is divisible by 3, console.log 'Dev'. If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'. If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
-
+  Create a for loop that iterates from 1 to 100. In this for loop, using some conditional logic,
+  // if the number your for loop is currently on is divisible by 3, console.log 'Dev'.
+  If the number is divisible by 5, console.log 'mountain'. If the number is divisible by 5 & 3, console.log 'Devmountain'.
+  If the number is not divisible by 5 or 3, console.log the number itself. Hint: Look up the modulo operator.
   Your output should look like:
   1
   2
@@ -176,5 +205,14 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
   16
   ...
 */
-
-//Code Here
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 ===0) {
+    console.log('Devmountain')
+  } else if (i % 5 === 0) {
+    console.log('mountain')
+  } else if (i % 3 === 0) {
+    console.log('dev')
+  } else {
+    console.log(i)
+  }
+}
